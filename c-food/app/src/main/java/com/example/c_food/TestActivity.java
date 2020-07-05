ce6fb9f07a55f96cb5bfb89f46106cb542ebba38
Bùi Tuấn Anh <metwaroi31@gmail.com>
2020-07-05 14:56:36 +0700

    appsync sdk integrated

--- c-food/app/src/main/java/com/example/c_food/TestActivity.java
+++ c-food/app/src/main/java/com/example/c_food/TestActivity.java
@@ -1,22 +1,30 @@
 package com.example.c_food;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.amazonaws.amplify.generated.graphql.CreateTodoMutation;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.Callback;
-import com.amazonaws.mobile.client.SignInUIOptions;
-import com.amazonaws.mobile.client.UserStateDetails;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.amazonaws.mobileconnectors.appsync.sigv4.CognitoUserPoolsAuthProvider;
 import com.amazonaws.mobileconnectors.dynamodbv2.document.Table;
 import com.amazonaws.mobileconnectors.dynamodbv2.document.datatype.Document;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+
+import javax.annotation.Nonnull;
+
+import type.CreateTodoInput;
 
 public class TestActivity extends AppCompatActivity {
     private String COGNITO_POOL_ID = "ap-southeast-1:549f3666-696b-4b65-8b7a-c615f955c41f";
@@ -24,20 +32,48 @@ public class TestActivity extends AppCompatActivity {
     private AmazonDynamoDBClient dbClient;
     private Table dbTable;
     private String TABLE_NAME = "testDB";
+    private AWSAppSyncClient mAWSAppSyncClient;
+    private GraphQLCall.Callback<CreateTodoMutation.Data> mutationCallback = new GraphQLCall.Callback<CreateTodoMutation.Data>() {
+        @Override
+        public void onResponse(@Nonnull Response<CreateTodoMutation.Data> response) {
+            Log.i("Results", "Added Todo");
+        }
+
+        @Override
+        public void onFailure(@Nonnull ApolloException e) {
+            Log.e("Error", e.toString());
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        connectDB testConnect =  new connectDB();
-        testConnect.execute();
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .cognitoUserPoolsAuthProvider(new CognitoUserPoolsAuthProvider() {
+                    @Override
+                    public String getLatestAuthToken() {
+                        try {
+                            return AWSMobileClient.getInstance().getTokens().getIdToken().getTokenString();
+                        } catch (Exception e){
+                            Log.e("APPSYNC_ERROR", e.getLocalizedMessage());
+                            return e.getLocalizedMessage();
+                        }
+                    }
+                }).build();
+
     }
 
     public void demoAdd(View view) {
-        Document newMemo = new Document();
-        newMemo.put("content", "a text");
-        newMemo.put("ID", 1);
-        CreateItemAsyncTask task = new CreateItemAsyncTask();
-        task.execute(newMemo);
+        CreateTodoInput createTodoInput = CreateTodoInput.builder()
+                .name("Use AppSync")
+                .description("Realtime and Offline")
+                .build();
+
+        mAWSAppSyncClient.mutate(CreateTodoMutation.builder().input(createTodoInput).build())
+                .enqueue(mutationCallback);
+
     }
     private class CreateItemAsyncTask extends AsyncTask<Document, Void, Void> {
         @Override
