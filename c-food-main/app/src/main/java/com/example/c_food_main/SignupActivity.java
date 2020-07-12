8d3dc06707fc6725d591f353f161b681e05ad835
Bùi Tuấn Anh <metwaroi31@gmail.com>
2020-07-12 14:47:02 +0700

    add user

--- c-food-main/app/src/main/java/com/example/c_food_main/SignupActivity.java
+++ c-food-main/app/src/main/java/com/example/c_food_main/SignupActivity.java
@@ -9,14 +9,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.amazonaws.amplify.generated.graphql.CreateFavoriteFoodMutation;
+import com.amazonaws.amplify.generated.graphql.CreateUserMutation;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.amplifyframework.auth.AuthUserAttributeKey;
 import com.amplifyframework.auth.options.AuthSignUpOptions;
 import com.amplifyframework.core.Amplify;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
 import com.example.c_food_main.model.User;
+
+import javax.annotation.Nonnull;
+
+import type.CreateUserInput;
 
 public class SignupActivity extends AppCompatActivity {
     EditText usernameEditText, passwordEditText, emailEditText;
     Button signUpBtn;
+    private AWSAppSyncClient mAWSAppSyncClient;
+    private GraphQLCall.Callback<CreateUserMutation.Data> mutationUserCallback = new GraphQLCall.Callback<CreateUserMutation.Data>() {
+        @Override
+        public void onResponse(@Nonnull Response<CreateUserMutation.Data> response) {
+            Log.i("Results", "Added Todo");
+//            Toast.makeText(getApplicationContext(), "User added", Toast.LENGTH_LONG).show();
+
+        }
+
+        @Override
+        public void onFailure(@Nonnull ApolloException e) {
+            Log.e("Error", e.toString());
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +56,7 @@ public class SignupActivity extends AppCompatActivity {
                 signUp();
             }
         });
+        initDatabase();
     }
     private void signUp() {
         String username, password, email;
@@ -44,6 +70,7 @@ public class SignupActivity extends AppCompatActivity {
                 result -> {
                     Log.i("AuthQuickStart", "Result: " + result.toString());
                     User newUser = new User(username,email);
+                    addUser(username, password, email);
                     goToConfirmCode(newUser);
                     },
                 error -> Log.e("AuthQuickStart", "Sign up failed", error)
@@ -53,5 +80,20 @@ public class SignupActivity extends AppCompatActivity {
         Intent intent = new Intent(SignupActivity.this,ConfirmCodeActivity.class);
         intent.putExtra("newUser",user);
         startActivity(intent);
+    }
+    private void addUser (String username, String password, String email) {
+        CreateUserInput createUserInput = CreateUserInput.builder()
+                                                .username(username)
+                                                .password(password)
+                                                .email(email).build();
+
+        mAWSAppSyncClient.mutate(CreateUserMutation.builder().input(createUserInput).build())
+                .enqueue(mutationUserCallback);
+    }
+    private void initDatabase () {
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
     }
 }
