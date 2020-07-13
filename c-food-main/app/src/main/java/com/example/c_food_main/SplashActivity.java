6f045f670cb5b7ff956c4c071ecdad12571d830e
Thuong <hoangthuongcmg@gmail.com>
2020-07-14 00:30:35 +0700

    UI Login , signup, splash

--- c-food-main/app/src/main/java/com/example/c_food_main/SplashActivity.java
+++ c-food-main/app/src/main/java/com/example/c_food_main/SplashActivity.java
@@ -1,42 +1,88 @@
+
 package com.example.c_food_main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.amplifyframework.auth.cognito.AWSCognitoAuthSession;
 import com.amplifyframework.core.Amplify;
 
-public class SplashActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+
+public class SplashActivity extends AppCompatActivity {
+    private static  int SPLASH_SCREEN = 5000;
+    Animation topAnim , bottomAnim;
+    ImageView image;
+    TextView logo , slogan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-        Amplify.Auth.fetchAuthSession(
-                result -> {
-                    AWSCognitoAuthSession cognitoAuthSession = (AWSCognitoAuthSession) result;
-                    switch(cognitoAuthSession.getIdentityId().getType()) {
-                        case SUCCESS:
-                            goToMainActivity();
-                            Log.i("AuthQuickStart", "IdentityId: " + cognitoAuthSession.getIdentityId().getValue());
-                            break;
-                        case FAILURE:
-                            goToLoginActivity();
-                            Log.i("AuthQuickStart", "IdentityId not present because: " + cognitoAuthSession.getIdentityId().getError().toString());
-                    }
-                },
-                error -> Log.e("AuthQuickStart", error.toString())
-        );
-    }
-    private void goToLoginActivity() {
-        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-        startActivity(intent);
-    }
-    private void goToMainActivity() {
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+       slogan.setAnimation(bottomAnim);
+
+       new Handler().postDelayed(new Runnable()
+       {
+           public void run ()
+           {
+               Intent  intent = new Intent(SplashActivity.this,Dashboard.class);
+
+               Pair[] pairs = new Pair[2];
+               pairs[0] = new Pair<View, String>(image, "logo_image");
+               pairs[1] = new Pair<View, String>(logo, "logo_text");
+               ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this,pairs);
+               startActivity(intent, options.toBundle());
+           }
+       }, SPLASH_SCREEN);
+
+     //   Amplify.Auth.fetchAuthSession(
+       //         result -> {
+       //             AWSCognitoAuthSession cognitoAuthSession = (AWSCognitoAuthSession) result;
+        //            switch(cognitoAuthSession.getIdentityId().getType()) {
+        //                case SUCCESS:
+           //                 goToMainActivity();
+           //                 Log.i("AuthQuickStart", "IdentityId: " + cognitoAuthSession.getIdentityId().getValue());
+           //                 break;
+           //             case FAILURE:
+           //                 goToLoginActivity();
+            //                Log.i("AuthQuickStart", "IdentityId not present because: " + cognitoAuthSession.getIdentityId().getError().toString());
+             //       }
+           //     },
+            //    error -> Log.e("AuthQuickStart", error.toString())
+     //   );
+  //  }
+
+  //  private void goToLoginActivity() {
+ //       Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+  //      startActivity(intent);
+  //  }
+  //  private void goToMainActivity() {
+  //      Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+   //     startActivity(intent);
+
     }
 }
