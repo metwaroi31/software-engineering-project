440363f9381eeb2c0a5df7b184f7e2d9a0060cf0
Bùi Tuấn Anh <metwaroi31@gmail.com>
2020-07-15 01:07:42 +0700

    PUSH QUERY

--- c-food-main/app/src/main/java/com/example/c_food_main/MainActivity.java
+++ c-food-main/app/src/main/java/com/example/c_food_main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.c_food_main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,12 +8,47 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.amazonaws.amplify.generated.graphql.CreateFoodMutation;
+import com.amazonaws.amplify.generated.graphql.ListFoodsQuery;
+import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthSession;
 import com.amplifyframework.auth.options.AuthSignOutOptions;
 import com.amplifyframework.core.Amplify;
+import com.apollographql.apollo.GraphQLCall;
+
+import com.amazonaws.amplify.generated.graphql.CreateFavoriteFoodMutation;
+import com.amazonaws.amplify.generated.graphql.CreateUserMutation;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.amplifyframework.auth.AuthUserAttributeKey;
+import com.amplifyframework.auth.options.AuthSignUpOptions;
+import com.amplifyframework.core.Amplify;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.example.c_food_main.model.User;
+
+import javax.annotation.Nonnull;
+
+import type.CreateFoodInput;
+import type.CreateUserInput;
 
 public class MainActivity extends AppCompatActivity {
+    private AWSAppSyncClient mAWSAppSyncClient;
+    private GraphQLCall.Callback<ListFoodsQuery.Data> todosCallback = new GraphQLCall.Callback<ListFoodsQuery.Data>() {
+        @Override
+        public void onResponse(@NonNull Response<ListFoodsQuery.Data> response) {
+            Log.i("Results of query", response.data().listFoods().items().toString());
+            Toast.makeText(getApplicationContext(), "query successfully", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onFailure(@NonNull ApolloException e) {
+            Log.i("ERROR OF QUERY", e.toString());
+        }
+    };
 
     Button logoutButton;
     @Override
@@ -20,7 +56,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logoutButton = findViewById(R.id.logout_btn);
-
+        initDatabase();
+        query();
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +77,16 @@ public class MainActivity extends AppCompatActivity {
     private void goToLoginActivity() {
         Intent intent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(intent);
+    }
+    private void initDatabase () {
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
+    }
+    public void query(){
+        mAWSAppSyncClient.query(ListFoodsQuery.builder().build())
+                .enqueue(todosCallback);
     }
 
 }
