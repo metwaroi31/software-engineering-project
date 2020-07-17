328a0518a91a41609663a4cd030fe50246b568bf
Thuong <hoangthuongcmg@gmail.com>
2020-07-17 19:04:45 +0700

    build Collpasing bar and design button

---
+++ c-food-main/app/src/main/java/com/example/c_food_main/activity_collapsing_toolbar.java
@@ -0,0 +1,14 @@
+package com.example.c_food_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_collapsing_toolbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing_toolbar);
+    }
+}
