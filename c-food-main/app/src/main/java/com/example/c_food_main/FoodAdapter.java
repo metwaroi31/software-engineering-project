9568a0215422f2d244db09eced0bfdc4015cd02d
Dinh Quoc Thuy <qthuy.0081@gmail.com>
2020-07-20 14:25:44 +0700

    integrate cardview with food detail

--- c-food-main/app/src/main/java/com/example/c_food_main/FoodAdapter.java
+++ c-food-main/app/src/main/java/com/example/c_food_main/FoodAdapter.java
@@ -1,6 +1,8 @@
 package com.example.c_food_main;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +23,7 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 
     private Context mContext;
     private Response<ListFoodsQuery.Data> foodList;
-
+    int currentPosition;
     FoodAdapter(Context context, Response<ListFoodsQuery.Data> list) {
         this.mContext = context;
         this.foodList = list;
@@ -42,19 +44,21 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+
         ImageView image = holder.item_image;
         TextView name, calories;
         Button likeBtn;
-
         name = holder.item_name;
         calories = holder.item_calories;
         likeBtn = holder.likeBtn;
-
-//        image.setImageResource(foodList.data().listFoods().items().get(position).getImage());
+        currentPosition = position;
+//      image.setImageResource(foodList.data().listFoods().items().get(position).getImage());
         name.setText(foodList.data().listFoods().items().get(position).name());
         calories.setText(Double.toString((foodList.data().listFoods().items().get(position).weight())));
-
+        Log.i("BindViewHolder",Integer.toString(currentPosition));
     }
+
+
 
     @Override
     public int getItemCount() {
@@ -62,18 +66,36 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
         ImageView item_image;
         TextView item_name, item_calories;
         Button likeBtn;
+        String food_id;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             item_image = itemView.findViewById(R.id.food_image);
-
             item_name = itemView.findViewById(R.id.food_name_text);
             item_calories = itemView.findViewById(R.id.food_calories);
             likeBtn = itemView.findViewById(R.id.like_food_btn);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int po;
+                    po = getAdapterPosition();
+                    Log.i("ViewHolderPosition", Integer.toString(po));
+                    String foodName = foodList.data().listFoods().items().get(po).name();
+                    String foodId = foodList.data().listFoods().items().get(po).id();
+                    Intent intent = new Intent(v.getContext(),FoodDetailActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra("Food_Name", foodName);
+                    intent.putExtra("Food_id",foodId);
+                    v.getContext().startActivity(intent);
+                }
+            });
         }
+
+
     }
 }
