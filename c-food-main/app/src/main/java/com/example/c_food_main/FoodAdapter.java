14e7f225bf3acc885d325dc9ec485013bc86f272
Bùi Tuấn Anh <metwaroi31@gmail.com>
2020-07-22 01:33:54 +0700

    run add fav food but no response

--- c-food-main/app/src/main/java/com/example/c_food_main/FoodAdapter.java
+++ c-food-main/app/src/main/java/com/example/c_food_main/FoodAdapter.java
@@ -14,21 +14,62 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amazonaws.amplify.generated.graphql.CreateFavoriteFoodMutation;
 import com.amazonaws.amplify.generated.graphql.ListFoodsQuery;
+import com.amazonaws.amplify.generated.graphql.ListUsersQuery;
+import com.amazonaws.amplify.generated.graphql.ListVitaminssQuery;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
+import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
+import com.amazonaws.mobileconnectors.cognitoauth.Auth;
+import com.amplifyframework.core.Amplify;
+import com.apollographql.apollo.GraphQLCall;
 import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
 
 
 import es.dmoral.toasty.Toasty;
+import type.CreateFavoriteFoodInput;
+import type.ModelIDInput;
+import type.ModelStringInput;
+import type.ModelUserFilterInput;
+import type.ModelVitaminsFilterInput;
 
 public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
-
+    private AWSAppSyncClient mAWSAppSyncClient;
+    private String currentUser;
     private Context mContext;
     private Response<ListFoodsQuery.Data> foodList;
     int currentPosition;
+    private GraphQLCall.Callback<ListUsersQuery.Data> userCallback = new GraphQLCall.Callback<ListUsersQuery.Data>() {
+        @Override
+        public void onResponse(@NonNull Response<ListUsersQuery.Data> response) {
+            response.data().listUsers().items().forEach(item -> {
+                Log.i("user", item.id());
+                currentUser = item.id();
+            });
+        }
+        @Override
+        public void onFailure(@NonNull ApolloException e) {
+            Log.i("ERROR OF QUERY", e.toString());
+        }
+    };
+    private GraphQLCall.Callback<CreateFavoriteFoodMutation.Data> favFoodCallback = new GraphQLCall.Callback<CreateFavoriteFoodMutation.Data>() {
+        @Override
+        public void onResponse(@NonNull Response<CreateFavoriteFoodMutation.Data> response) {
+            Log.i("fav food", "added" + " " + response.data().toString());
+
+        }
+        @Override
+        public void onFailure(@NonNull ApolloException e) {
+            Log.i("ERROR OF QUERY", e.toString());
+        }
+    };
     FoodAdapter(Context context, Response<ListFoodsQuery.Data> list) {
         this.mContext = context;
         this.foodList = list;
-
+        initDatabase();
+        queryUser();
     }
     @NonNull
     @Override
@@ -99,9 +140,35 @@ public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
                 public void onClick(View v) {
                     int currentPosition = getAdapterPosition();
                     String foodName = foodList.data().listFoods().items().get(currentPosition).name();
+                    String foodId = foodList.data().listFoods().items().get(currentPosition).id();
+                    addFavFood(foodId, currentUser);
                     Toasty.success(v.getContext(), "Add " + foodName + " into favorite food" , Toast.LENGTH_SHORT, true).show();
                 }
             });
         }
+    }
+    private void initDatabase () {
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(mContext)
+                .awsConfiguration(new AWSConfiguration(mContext))
+                .build();
+    }
+    private void addFavFood (String foodID, String userID) {
+        CreateFavoriteFoodInput createFavoriteFoodInput = CreateFavoriteFoodInput.builder()
+                                                            .foodID(foodID)
+                                                            .userID(userID)
+                                                            .build();
+        mAWSAppSyncClient.mutate(CreateFavoriteFoodMutation.builder().input(createFavoriteFoodInput).build())
+                .enqueue(favFoodCallback);
+    }
+    private void queryUser () {
+        Log.i("user", Amplify.Auth.getCurrentUser().getUsername());
+        String username = Amplify.Auth.getCurrentUser().getUsername();
+        ModelStringInput modelStringInput = ModelStringInput.builder().eq(username).build();
+        ModelUserFilterInput modelFoodFilterInput = ModelUserFilterInput.builder().username(modelStringInput).build();
+        mAWSAppSyncClient.query(ListUsersQuery.builder().filter(modelFoodFilterInput ).build())
+                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+                .enqueue(userCallback);
+
     }
 }
