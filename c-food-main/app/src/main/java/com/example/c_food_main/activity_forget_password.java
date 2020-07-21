641c11ebfa4f1fddf0512046a38cb35d02fa3f56
Thuong <hoangthuongcmg@gmail.com>
2020-07-21 18:07:27 +0700

    design forgetpassword vs newpassword

---
+++ c-food-main/app/src/main/java/com/example/c_food_main/activity_forget_password.java
@@ -0,0 +1,14 @@
+package com.example.c_food_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_forget_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
