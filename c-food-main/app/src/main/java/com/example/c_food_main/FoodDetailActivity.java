a19d556033019e3b81dd803755eb48b45b3b5a72
Bùi Tuấn Anh <metwaroi31@gmail.com>
2020-07-22 01:08:09 +0700

    get all the food information

--- c-food-main/app/src/main/java/com/example/c_food_main/FoodDetailActivity.java
+++ c-food-main/app/src/main/java/com/example/c_food_main/FoodDetailActivity.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.amazonaws.amplify.generated.graphql.ListFoodsQuery;
+import com.amazonaws.amplify.generated.graphql.ListIngredientsFoodsQuery;
+import com.amazonaws.amplify.generated.graphql.ListIngredientssQuery;
 import com.amazonaws.amplify.generated.graphql.ListMacronutrientsFoodsQuery;
 import com.amazonaws.amplify.generated.graphql.ListMacronutrientssQuery;
+import com.amazonaws.amplify.generated.graphql.ListVitaminsFoodsQuery;
+import com.amazonaws.amplify.generated.graphql.ListVitaminssQuery;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
@@ -17,18 +21,26 @@ import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import type.ModelFoodFilterInput;
 import type.ModelIDInput;
+import type.ModelIngredientsFilterInput;
+import type.ModelIngredientsFoodFilterInput;
 import type.ModelMacronutrientsFilterInput;
 import type.ModelMacronutrientsFoodFilterInput;
 import type.ModelStringInput;
+import type.ModelVitaminsFilterInput;
+import type.ModelVitaminsFoodFilterInput;
 
 public class FoodDetailActivity extends AppCompatActivity {
     private AWSAppSyncClient mAWSAppSyncClient;
     CollapsingToolbarLayout collapsingToolbarLayout;
-    private List<String> macronutrients;
+    private ArrayList<String> macronutrients = new ArrayList<String>();
+    private ArrayList<String> vitamins = new ArrayList<String>();
+    private ArrayList<String> ingredients = new ArrayList<String>();
+    private Response<ListMacronutrientssQuery.Data> macrotemp;
     private GraphQLCall.Callback<ListMacronutrientssQuery.Data> macroCallback = new GraphQLCall.Callback<ListMacronutrientssQuery.Data>() {
         @Override
         public void onResponse(@NonNull Response<ListMacronutrientssQuery.Data> response) {
@@ -46,10 +58,63 @@ public class FoodDetailActivity extends AppCompatActivity {
     private GraphQLCall.Callback<ListMacronutrientsFoodsQuery.Data> macroFoodCallback = new GraphQLCall.Callback<ListMacronutrientsFoodsQuery.Data>() {
         @Override
         public void onResponse(@NonNull Response<ListMacronutrientsFoodsQuery.Data> response) {
-            Log.i("macroID" , "run");
-            response.data().listMacronutrientsFoods().items().forEach(item -> {
+             response.data().listMacronutrientsFoods().items().forEach(item -> {
                 Log.i("macroID", item.macronutrientID() );
                 queryMacro(item.macronutrientID());
+            });
+        }
+        @Override
+        public void onFailure(@NonNull ApolloException e) {
+            Log.i("ERROR OF QUERY", e.toString());
+        }
+    };
+    private GraphQLCall.Callback<ListVitaminssQuery.Data> vitaminCallback = new GraphQLCall.Callback<ListVitaminssQuery.Data>() {
+        @Override
+        public void onResponse(@NonNull Response<ListVitaminssQuery.Data> response) {
+            response.data().listVitaminss().items().forEach(item -> {
+                String vitaminToOutput = item.name() + " " + item.value() + " " + item.unit();
+                Log.i("vitaminID", vitaminToOutput );
+                vitamins.add(vitaminToOutput);
+            });
+        }
+        @Override
+        public void onFailure(@NonNull ApolloException e) {
+            Log.i("ERROR OF QUERY", e.toString());
+        }
+    };
+    private GraphQLCall.Callback<ListVitaminsFoodsQuery.Data> vitaminFoodCallback = new GraphQLCall.Callback<ListVitaminsFoodsQuery.Data>() {
+        @Override
+        public void onResponse(@NonNull Response<ListVitaminsFoodsQuery.Data> response) {
+            response.data().listVitaminsFoods().items().forEach(item -> {
+                Log.i("vitaminID", item.vitaminID() );
+                queryVitamins(item.vitaminID());
+            });
+        }
+        @Override
+        public void onFailure(@NonNull ApolloException e) {
+            Log.i("ERROR OF QUERY", e.toString());
+        }
+    };
+    private GraphQLCall.Callback<ListIngredientssQuery.Data> ingredientCallback = new GraphQLCall.Callback<ListIngredientssQuery.Data>() {
+        @Override
+        public void onResponse(@NonNull Response<ListIngredientssQuery.Data> response) {
+            response.data().listIngredientss().items().forEach(item -> {
+                String ingredientToOutput = item.name() + " " + item.isvegeterian();
+                Log.i("vitaminID", ingredientToOutput );
+                ingredients.add(ingredientToOutput);
+            });
+        }
+        @Override
+        public void onFailure(@NonNull ApolloException e) {
+            Log.i("ERROR OF QUERY", e.toString());
+        }
+    };
+    private GraphQLCall.Callback<ListIngredientsFoodsQuery.Data> ingredientFoodCallback = new GraphQLCall.Callback<ListIngredientsFoodsQuery.Data>() {
+        @Override
+        public void onResponse(@NonNull Response<ListIngredientsFoodsQuery.Data> response) {
+            response.data().listIngredientsFoods().items().forEach(item -> {
+                Log.i("vitaminID", item.ingredientID() );
+                queryVitamins(item.ingredientID());
             });
         }
         @Override
@@ -66,8 +131,13 @@ public class FoodDetailActivity extends AppCompatActivity {
         String position = getIntent().getStringExtra("Position");
         collapsingToolbarLayout = findViewById(R.id.collapsingToolBar1);
         collapsingToolbarLayout.setTitle(foodName + foodID);
+        initVariables();
         initDatabase();
         queryMacrosOfFood(foodID);
+        queryVitaminsOfFood(foodID);
+    }
+    private void initVariables() {
+
     }
     private void initDatabase () {
         mAWSAppSyncClient = AWSAppSyncClient.builder()
@@ -84,11 +154,38 @@ public class FoodDetailActivity extends AppCompatActivity {
                 .enqueue(macroCallback);
     }
     private void queryMacrosOfFood(String foodID) {
-        Log.i("foodID", foodID);
         ModelIDInput modelStringInput = ModelIDInput.builder().contains(foodID).build();
         ModelMacronutrientsFoodFilterInput modelFoodFilterInput = ModelMacronutrientsFoodFilterInput.builder().foodID(modelStringInput).build();
         mAWSAppSyncClient.query(ListMacronutrientsFoodsQuery.builder().filter(modelFoodFilterInput ).build())
                 .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
                 .enqueue(macroFoodCallback);
+    }
+    private void queryVitamins (String vitaminID) {
+        ModelIDInput modelStringInput = ModelIDInput.builder().contains(vitaminID).build();
+        ModelVitaminsFilterInput modelFoodFilterInput = ModelVitaminsFilterInput.builder().id(modelStringInput).build();
+        mAWSAppSyncClient.query(ListVitaminssQuery.builder().filter(modelFoodFilterInput ).build())
+                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+                .enqueue(vitaminCallback);
+    }
+    private void queryVitaminsOfFood(String foodID) {
+        ModelIDInput modelStringInput = ModelIDInput.builder().contains(foodID).build();
+        ModelVitaminsFoodFilterInput modelFoodFilterInput = ModelVitaminsFoodFilterInput.builder().foodID(modelStringInput).build();
+        mAWSAppSyncClient.query(ListVitaminsFoodsQuery.builder().filter(modelFoodFilterInput ).build())
+                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+                .enqueue(vitaminFoodCallback);
+    }
+    private void queryIngredients (String ingredientID) {
+        ModelIDInput modelStringInput = ModelIDInput.builder().contains(ingredientID).build();
+        ModelIngredientsFilterInput modelFoodFilterInput = ModelIngredientsFilterInput.builder().id(modelStringInput).build();
+        mAWSAppSyncClient.query(ListIngredientssQuery.builder().filter(modelFoodFilterInput ).build())
+                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+                .enqueue(ingredientCallback);
+    }
+    private void queryIngredientsOfFood(String foodID) {
+        ModelIDInput modelStringInput = ModelIDInput.builder().contains(foodID).build();
+        ModelIngredientsFoodFilterInput modelFoodFilterInput = ModelIngredientsFoodFilterInput.builder().foodID(modelStringInput).build();
+        mAWSAppSyncClient.query(ListIngredientsFoodsQuery.builder().filter(modelFoodFilterInput ).build())
+                .responseFetcher(AppSyncResponseFetchers.CACHE_AND_NETWORK)
+                .enqueue(ingredientFoodCallback);
     }
 }
