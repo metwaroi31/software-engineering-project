bdf12deb099d0504783344dc2ae73b667bb99fc0
Dinh Quoc Thuy <qthuy.0081@gmail.com>
2020-07-23 16:29:50 +0700

    build cover image of food details

--- c-food-main/app/src/main/java/com/example/c_food_main/activity/FoodDetailActivity.java
+++ c-food-main/app/src/main/java/com/example/c_food_main/activity/FoodDetailActivity.java
@@ -4,21 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.c_food_main.R;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class FoodDetailActivity extends AppCompatActivity {
     CollapsingToolbarLayout collapsingToolbarLayout;
+    ImageView coverImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_detail);
+        coverImage = findViewById(R.id.food_cover_image);
         String foodName = getIntent().getStringExtra("Food_Name");
+        int image = getIntent().getIntExtra("Food_Image",0);
         String foodID = getIntent().getStringExtra("Food_id");
         String position = getIntent().getStringExtra("Position");
         collapsingToolbarLayout = findViewById(R.id.collapsingToolBar1);
-        collapsingToolbarLayout.setTitle(foodName + foodID);
+        collapsingToolbarLayout.setTitle(foodName);
+
+        coverImage.setImageResource(image);
     }
 
 }
